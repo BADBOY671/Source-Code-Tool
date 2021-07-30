99fd9ff297d054463d526263f3c048489a1880d5
BADBOY671 <86197607+BADBOY671@users.noreply.github.com>
2021-07-30 03:24:36 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-import requests
+import requests, time
 print('Enter your Website Url:')
 x = input()
 
@@ -6,6 +6,5 @@ URL = x
 page = requests.get(URL)
 
 print('\x1b[6;30;42m' + 'Scan....!' + '\x1b[0m')
-
-    
+time.sleep(5)
 print(page.text)
